573fc4ebf1305cf643092369c0ed8a7a2e996e88
Clark Guan <guangongbo@meituan.com>
2020-09-21 00:47:16 +0800

    解决中文乱码问题

--- ui/main_unix.go
+++ ui/main_unix.go
@@ -2,8 +2,25 @@
 
 package main
 
-import "fmt"
+import (
+	"fyne.io/fyne/app"
+	"fyne.io/fyne/dialog"
+	"fyne.io/fyne/widget"
+)
 
 func main() {
-	fmt.Println("hello world")
+	a := app.New()
+	w := a.NewWindow("金庸群侠传修改器")
+	w.SetContent(widget.NewVBox(
+		widget.NewButton("存档 1", func() {
+			dialog.ShowConfirm("提醒", "您点击了存档 1", nil, w)
+		}),
+		widget.NewButton("存档 2", func() {
+			dialog.ShowConfirm("提醒", "您点击了存档 2", nil, w)
+		}),
+		widget.NewButton("存档 3", func() {
+			dialog.ShowConfirm("提醒", "您点击了存档 3", nil, w)
+		}),
+	))
+	w.ShowAndRun()
 }
