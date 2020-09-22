d7887bb38f54785b99f6631de9f49bef0baff7fd
whoami <wdsgyj@gmail.com>
2020-09-23 01:31:16 +0800

    init

--- ui/main_unix.go
+++ ui/main_unix.go
@@ -3,8 +3,11 @@
 package main
 
 import (
+	"fmt"
+	"strings"
 	"time"
 
+	"../conf"
 	"fyne.io/fyne"
 	"fyne.io/fyne/app"
 	"fyne.io/fyne/dialog"
@@ -14,11 +17,23 @@ import (
 func main() {
 	a := app.NewWithID("金庸群侠传修改器")
 	w := a.NewWindow("金庸群侠传修改器")
-	w.SetContent(widget.NewTabContainer(
-		widget.NewTabItem("存档 1", newPageContent(w)),
-		widget.NewTabItem("存档 2", newPageContent(w)),
-		widget.NewTabItem("存档 3", newPageContent(w)),
-	))
+	w.Resize(fyne.Size{Width: 320, Height: 480})
+	entry := widget.NewEntry()
+	execPath, _ := conf.ExecutablePath()
+	entry.SetText(execPath)
+	form := widget.NewForm(
+		widget.NewFormItem("安装位置：", entry),
+	)
+	form.SubmitText = "确定"
+	form.OnSubmit = func() {
+		path, err := conf.SavesPath(strings.TrimSpace(entry.Text))
+		if err != nil {
+			dialog.NewError(err, w).Show()
+			return
+		}
+		fmt.Println(path)
+	}
+	w.SetContent(form)
 	w.ShowAndRun()
 }
 
@@ -29,7 +44,7 @@ func newPageContent(parent fyne.Window) fyne.CanvasObject {
 		widget.NewFormItem("功夫带毒:", widget.NewEntry()),
 		widget.NewFormItem("左右互搏:", widget.NewCheck("", nil)),
 		widget.NewFormItem("资质:", widget.NewEntry()),
-		widget.NewFormItem("武功1:", widget.NewSelect([]string{"野球拳", "普通攻击"}, nil)),
+		widget.NewFormItem("武功1:", widget.NewSelect(conf.Gongfu, nil)),
 		widget.NewFormItem("武功1经验:", widget.NewEntry()),
 	)
 	form.SubmitText = "修改"
